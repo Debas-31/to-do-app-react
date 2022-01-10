89e91b0e6714c53b63e1689497797c78661eee33
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2022-01-10 14:51:27 +0300

    Delete src content

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+// component file
+import TodoContainer from './components/TodoContainer';
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <TodoContainer />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
